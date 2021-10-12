7faa6e0c2939176e88c5d5284ba1a7b6deaad3f2
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-10-11 22:59:57 -0700

    update madlib

--- unit19/unit19_2/app.py
+++ unit19/unit19_2/app.py
@@ -4,20 +4,17 @@ from stories import story
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "secretzkeyz123"
+app.config['SECRET_KEY'] = "secrets"
 debug = DebugToolbarExtension(app)
 
 
 @app.route('/')
 def home_page():
-    # print(story.prompts, flush=True)
     '''Shows home page'''
     return render_template('home.html', prompts=story.prompts)
 
 
 @app.route('/story')
 def story_page():
-    # answers = {prompt:request.args[prompt] for prompt in story.prompts }
-    # this_story = story.generate(answers)
     text = story.generate(request.args)
     return render_template('story.html', text=text)
