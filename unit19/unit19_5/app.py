9a1a7b2765dea3f1a8cd2a54b5783e7068384c2a
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-10-19 22:12:05 -0700

    updated

--- unit19/unit19_5/app.py
+++ unit19/unit19_5/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "fdfgkjtjkkg45yfdb"
+app.config["SECRET_KEY"] = "a-secret"
 
 boggle_game = Boggle()
 
