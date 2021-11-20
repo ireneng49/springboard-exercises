d346ad4213cf909edbec88b42d5b776faf5f07ba
Irene Ng <30131788+ireneng49@users.noreply.github.com>
2021-11-19 16:23:10 -0800

    outer_space_update

--- unit22/unit22_4/outer_space.sql
+++ unit22/unit22_4/outer_space.sql
@@ -1,78 +1,79 @@
 -- from the terminal run:
 -- psql < outer_space.sql
 
-DROP DATABASE IF EXISTS outer_space;
-
-CREATE DATABASE outer_space;
-
-\c outer_space
-
-CREATE TABLE moon (
-  id SERIAL PRIMARY KEY,
-  moon_name TEXT
-  orbiting_planet INTEGER REFERENCES planet ON DELETE CASCADE
-);
-
-CREATE TABLE galaxy (
-  id SERIAL PRIMARY KEY,
-  galaxy_name TEXT NOT NULL
-);
-
-CREATE TABLE orbital_star (
-  id SERIAL PRIMARY KEY,
-  orbital_star_name TEXT
-);
-
-CREATE TABLE planet
+CREATE TABLE orbit_stars
 (
   id SERIAL PRIMARY KEY,
-  planet_id INT NOT NULL,
-  orbital_period_in_year FLOAT NOT NULL,
-  orbits_around INT REFERENCES orbital_star ON DELETE SET NULL,
-  galaxy INTEGER REFERENCES galaxy ON DELETE NULL
+  star_name TEXT UNIQUE NOT NULL
 );
 
-CREATE TABLE planet (
+CREATE TABLE galaxies
+(
   id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
+  galaxy TEXT UNIQUE NOT NULL
 );
 
-INSERT INTO planet (name)
-VALUES ('Earth'), ('Mars'), ('Neptune'), ('Venus'), 
-('Proxima Centauri b'), ('Gliese 876 b');
+CREATE TABLE planets
+(
+  id SERIAL PRIMARY KEY,
+  planet_name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  moons TEXT[]
+);
 
-INSERT INTO planet_info
-  (planet_id, orbital_period_in_years, orbits_around_planet_id, galaxy_id, moons_id)
+CREATE TABLE planet_galaxies
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER NOT NULL REFERENCES planets,
+  galaxy_id INTEGER NOT NULL REFERENCES galaxies
+);
+
+CREATE TABLE planet_orbits
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER NOT NULL REFERENCES planets,
+  orbit_star_id INTEGER NOT NULL REFERENCES orbit_stars
+);
+
+INSERT INTO orbit_stars
+(star_name)
 VALUES
-  (1, 1.00, 1, 1, 1),
-  (2, 1.88, 1, 1, 2),
-  (2, 1.88, 1, 1, 3),
-  (3, 164.8, 1, 1, 4),
-  (3, 164.8, 1, 1, 5),
-  (3, 164.8, 1, 1, 6),
-  (3, 164.8, 1, 1, 7),
-  (3, 164.8, 1, 1, 8),
-  (3, 164.8, 1, 1, 9),
-  (3, 164.8, 1, 1, 10),
-  (3, 164.8, 1, 1, 11),
-  (3, 164.8, 1, 1, 12),
-  (3, 164.8, 1, 1, 13),
-  (3, 164.8, 1, 1, 14),
-  (3, 164.8, 1, 1, 15),
-  (3, 164.8, 1, 1, 16),
-  (3, 164.8, 1, 1, 17);
-  
-  INSERT INTO planet_info
-  (planet_id, orbital_period_in_years, orbits_around_planet_id, galaxy_id)
-  VALUES (4, 0.62, 1, 1), (5, 0.03, 2, 1), (6, 0.23, 3, 1); 
+('The Sun'),
+('Proxima Centauri'),
+('Gliese 876');
 
-  INSERT INTO moon (moon_name)
-  VALUES ('The Moon'), ('Phobos'), ('Deimos'), ('Naiad'), ('Thalassa'), ('Despina'), ('Galatea'), 
-  ('Larissa'), ('Hippocamp'), ('Proteus'), ('Triton'), ('Nereid'), ('Halimede'),
-  ('Sao'), ('Laomedeia'), ('Psamathe'), ('Neso');
+INSERT INTO galaxies
+(galaxy)
+VALUES
+('Milky Way'),
+('Other Galaxy');
 
-  INSERT INTO galaxy (galaxy_name)
-  VALUES ('Milky Way');
+INSERT INTO planets
+  (planet_name, orbital_period_in_years, moons)
+VALUES
+  ('Earth', 1.00, '{"The Moon"}'),
+  ('Mars', 1.88, '{"Phobos", "Deimos"}'),
+  ('Venus', 0.62, '{}'),
+  ('Neptune', 164.8, '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "Hippocamp", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+  ('Proxima Centauri b', 0.03, '{}'),
+  ('Gliese 876 b', 0.23, '{}');
 
-  INSERT INTO orbits_around_planet(orbits_around_planet_name)
-  VALUES ('The Sun'), ('Proxima Centauri'), ('Gliese 876');
+INSERT INTO planet_galaxies
+(planet_id, galaxy_id)
+VALUES 
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1);
+
+INSERT INTO planet_orbits
+(planet_id, orbit_star_id)
+VALUES 
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 2),
+(6, 3);
